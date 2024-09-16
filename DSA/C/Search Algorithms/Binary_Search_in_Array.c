708e80af3b0f7fd0774dbfab03aa2cb7be72912c
XeRush <anonymoushere127001@gmail.com>
2024-09-16 12:50:38 +0530

    ./conf changed err0

--- DSA/C/Search Algorithms/Binary_Search_in_Array.c
+++ DSA/C/Search Algorithms/Binary_Search_in_Array.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int num, arr[num];
+    int num;
     
     printf("No. of Elements:");
     scanf("%d", &num);
+
+    int arr[num];
     
+    printf("Enter the elements:");
     for(int i=0; i<num; i++ ) {
-        printf("Enter the element:");
         scanf("%d", &arr[i]);
     }
 
-    int len = sizeof(arr), target;
+    int target, k=0;
 
-    int low = 0, high = len;
+    int low = 0, high = num;
     int mid = (high-low)/2;
 
     printf("Enter the target:");
@@ -21,16 +23,24 @@ int main() {
     for(int j=0; j<num; j++) {
         
         if (arr[mid] == target) {
-            printf("%d", arr[mid]);
+            printf("Element %d found\n", arr[mid]);
+            k++;
+            printf("%d sessions executed\n", k);
             break;
         }
 
         else if (arr[mid] < target) {
             low = mid + 1;
+            mid = (high+low)/2;
+            k++;
+            continue;
         }
         
         else if (arr[mid] > target) {
             high = mid - 1;
+            mid = (high+low)/2;
+            k++;
+            continue;
         }
     }
 }
